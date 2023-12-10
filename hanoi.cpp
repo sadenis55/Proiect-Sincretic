cedc913bb44a0c697dad052a03c0769109a382bb
lamulfex <denis.sarca03@gmail.com>
2023-12-10 15:05:42 +0200

    now com

--- hanoi.cpp
+++ hanoi.cpp
@@ -1,2 +1,29 @@
-// de scris codul!
+#include <iostream>
 
+using namespace std;
+
+void hanoi(int n, char a, char b, char c)
+{
+    if(n <= 0)
+    {
+        perror("Nu sunt discuri!");
+        exit(EXIT_FAILURE);
+    }
+    else if(n == 1)
+        cout<<a<<" -> "<<c<<"\n";
+    else
+    {
+        hanoi(n-1, a, c, b);
+        cout<<a<<" -> "<<c<<"\n";
+        hanoi(n-1, b, a, c);
+    }
+}
+
+int main()
+{
+    int n;
+    cout<<"Introduceti numarul de discuri: ";
+    cin>>n;
+    hanoi(n, 'A', 'B', 'C');
+    return 0;
+}
